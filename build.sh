ed7f27e4abfd9c1feeed9a585f07ede6b0bed14d
Wataru Maeda <w.maeda@neogenia.co.jp>
2024-03-31 22:15:51 +0900

    feat: ディレクトリ構成を見直し

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ IMAGE_NAME=neogenia/ez-gate
 NAME_TAG=$IMAGE_NAME:$TAG
 echo building image "$NAME_TAG" ...
 
-(cd $SCRIPT_DIR/docker; time docker build -t $NAME_TAG .) \
+(cd $SCRIPT_DIR/src; time docker build -t $NAME_TAG .) \
 && cat <<GUIDE
 # build finished successfuly.
 # If you push image to DockerHub, use below command:

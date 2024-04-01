4d2bc09f03f12fe79d557e1fece6fd41f944f938
Wataru Maeda <w.maeda@neogenia.co.jp>
2024-04-01 18:55:29 +0900

    feat: build スクリプトにビルドコマンドのオプションを付与できる様にした

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@
 SCRIPT_DIR=${SCRIPT_DIR:-$(cd $(dirname $0) && pwd)}
 
 TAG=${1:-`date "+%Y%m%d"`}
+shift
 
 IMAGE_NAME=neogenia/ez-gate
 NAME_TAG=$IMAGE_NAME:$TAG
 echo building image "$NAME_TAG" ...
 
-(cd $SCRIPT_DIR/src; time docker build -t $NAME_TAG .) \
+(cd $SCRIPT_DIR/src; time docker build -t $NAME_TAG . $@) \
 && cat <<GUIDE
 # build finished successfuly.
 # If you push image to DockerHub, use below command:
